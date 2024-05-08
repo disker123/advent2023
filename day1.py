59ab116f7b5663cfc727ef42998bcec835479797
root <root@DESKTOP-EA1IA1K.localdomain>
2024-05-07 20:44:38 -0400

    day1comeplte

--- day1.py
+++ day1.py
@@ -1,1 +1,19 @@
-print("hello world")
+File = open("day1input.txt", "r")
+data = File.readlines()
+#print(data)
+result = 0
+for i in data:
+    first = None
+    second = None
+
+    for j in i:
+        if(j.isnumeric()):
+            first = j
+            break
+    for j in reversed(i):
+        if(j.isnumeric()):
+            second = j
+            break
+    value = first + second
+    result += int(value)
+print(result)
