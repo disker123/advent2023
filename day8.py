fed80990a109e4722526e850235ef7f06c8dd2c8
root <root@DESKTOP-EA1IA1K.localdomain>
2024-07-06 14:19:47 -0400

    completed day 8

---
+++ day8.py
@@ -0,0 +1,28 @@
+File = open("day8input.txt", "r")
+data = File.readlines()
+
+directions = data[0]
+directions = directions.rstrip()
+del data[0]
+del data[0]
+
+wasteland = {}
+for line in data:
+    key = line[0:3]
+    paths = (line[7:10], line[12:15])
+    wasteland[key] = paths
+
+key = "AAA" #start key
+counter = 0
+while(key != "ZZZ"):
+    for x in directions:
+        if(x == "L"):
+            key = wasteland[key][0]
+        if(x == "R"):
+            key = wasteland[key][1]
+        counter += 1
+        #print(x)
+
+        if(key == "ZZZ"):
+            break
+print(counter)
